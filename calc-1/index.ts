a033aca3c085e25291e65043d7efb066db34e7b4
Riday Shah <78732161+heart2001@users.noreply.github.com>
2022-05-09 21:45:50 +0530

    Create index.ts

---
+++ calc-1/index.ts
@@ -0,0 +1,18 @@
+console.log("*****Calculator Example*****");
+function add(x: number, y: number) {
+  console.log("Addition: ", x + y);
+}
+function sub(x: number, y: number) {
+  console.log("Subtraction: ", x - y);
+}
+function mul(x: number, y: number) {
+  console.log("Multiplication: ", x * y);
+}
+function div(x: number, y: number) {
+  console.log("Division: ", x / y);
+}
+
+add(20, 30);
+sub(10, 5);
+mul(6, 3);
+div(8, 2);
