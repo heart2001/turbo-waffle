2604aaf5b1d882858647f83a672189121f8ef8c0
Riday Shah <78732161+heart2001@users.noreply.github.com>
2022-05-09 22:27:18 +0530

    Create index.ts

---
+++ inheritance/index.ts
@@ -0,0 +1,22 @@
+console.log('*****Inheritance Example*****');
+
+class Animal {
+	eat(): void {
+		console.log('Eating');
+	}
+}
+class Dog extends Animal {
+	bark(): void {
+		console.log('Barking');
+	}
+}
+class BabyDog extends Dog {
+	weep(): void {
+		console.log('Weeping');
+	}
+}
+
+const obj = new BabyDog();
+obj.eat();
+obj.bark();
+obj.weep();
